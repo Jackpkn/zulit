632a725a58fd96cff72e2163c91f97af768e5497
Jackpkn <jackpkn07@gmail.com>
2024-11-05 07:39:49 +0530

    Form field added

---
+++ packages/form_field/test/src/form_field_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:form_field/form_field.dart';
+
+void main() {
+  group('FormField', () {
+    test('can be instantiated', () {
+      // expect(FormField(), isNotNull);
+    });
+  });
+}
